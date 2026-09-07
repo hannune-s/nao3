efe7ba2fd2dcab85ab1a7e30752d4ca1b39b52ff
hannune-s <wkhn11@gmail.com>
2026-09-07 11:42:56 +0900

    style: remove black border from mobile mockup in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,8 +161,8 @@ export default function LandingPage() {
             
             {/* CSS UI Mockup - Customer Mobile */}
             <div className="relative max-w-[340px] mx-auto w-full lg:mr-0">
-              <div className="relative rounded-[3rem] bg-gray-900 p-3 shadow-[0_40px_80px_rgba(0,0,0,0.2)] border-4 border-gray-800 transform hover:scale-[1.02] transition-all duration-500">
-                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-32 h-6 bg-gray-900 rounded-b-2xl z-20"></div>
+              <div className="relative rounded-[2.5rem] bg-white p-2 shadow-[0_30px_80px_rgba(0,0,0,0.12)] border border-gray-100 transform hover:scale-[1.02] transition-all duration-500">
+                
                 <div className="bg-[#FAFAFA] rounded-[2.5rem] overflow-hidden h-[700px] relative flex flex-col no-scrollbar">
                   
                   {/* Header */}
